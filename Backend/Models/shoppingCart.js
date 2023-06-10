9a6f17fb0163d86db740522c10ca271965c0bddd
frnvnd4 <fernanda.lorca_b@mail.udp.cl>
2023-06-10 19:42:37 -0400

    carrito y order
    arreglo del stock del carrito y creacion de ordenes de compra

--- Backend/Models/shoppingCart.js
+++ Backend/Models/shoppingCart.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const cartschema = mongoose.Schema({
   User: String,
   items: [{
-    TitleProduct: String ,
-    Quantify: { type: Number, default: 1 },
+    TitleProduct:  { type: String, required: true },
+    Size : { type: String, required: true },
+    Quantity: { type: Number, default: 1 },
     price: { type: Number, required: true },
   }],
   total: { type: Number, default:0, required: true },
