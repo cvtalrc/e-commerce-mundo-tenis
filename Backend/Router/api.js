5b70ec8f5f93a7525abcd73dc924a8e805c43b71
frnvnd4 <fernanda.lorca_b@mail.udp.cl>
2023-06-25 20:59:10 -0400

    Update api.js

--- Backend/Router/api.js
+++ Backend/Router/api.js
@@ -15,36 +15,36 @@ api.use(cookieParser());
 api.post("/sign-up", func_auth.sign_up); //registrar
 api.post("/sign-in", func_auth.sign_in); //login
 api.post("/sign-out", func_auth.authenticateToken, func_auth.sign_out); //salir
-api.post("/product/", func_product.add);
+api.post("/product/", func_auth.authenticateToken, func_product.add);
 api.post("/cart", func_auth.authenticateToken, func_shoppingCart.createEmpty_shoppingCart); //ahora solo se crea el carro cuando se registra el usuario
 api.post("/cart/add", func_auth.authenticateToken, func_shoppingCart.addtoCart);
 api.post("/order/", func_auth.authenticateToken, func_order.createOrder);
 api.post("/comment/", func_auth.authenticateToken, func_comment.createComment);
 
 //API GET
-api.get("/user/", func_user.getAll);
+api.get("/user/", func_auth.authenticateToken, func_user.getAll);
 api.get("/user/:id", func_auth.authenticateToken, func_user.getUser);
-api.get("/product/", func_product.getAll);
-api.get("/product/:id", func_product.getId);
+api.get("/product/", func_auth.authenticateToken, func_product.getAll);
+api.get("/product/:id", func_auth.authenticateToken, func_product.getId);
 api.get("/cart/:user", func_auth.authenticateToken, func_shoppingCart.getCart);
 api.get("/order/:id", func_auth.authenticateToken, func_order.getOrder);
-api.get("/order/", func_order.getAll);
-api.get("/comment/", func_comment.getAllComments);
+api.get("/order/", func_auth.authenticateToken, func_order.getAll);
+api.get("/comment/", func_auth.authenticateToken, func_comment.getAllComments);
 
 //API DELETE
 api.delete("/user/remove/:id", func_auth.authenticateToken, func_user.removeUser)
-api.delete("/user/removeAll", func_user.removeAll);
-api.delete("/product/:id", func_product.remove);
-api.delete("/product/removeAll", func_product.removeAll);
-api.delete("/cart/removeAll", func_shoppingCart.emptyCart);
+api.delete("/user/removeAll", func_auth.authenticateToken, func_user.removeAll);
+api.delete("/product/:id", func_auth.authenticateToken, func_product.remove);
+api.delete("/product/removeAll", func_auth.authenticateToken, func_product.removeAll);
+api.delete("/cart/removeAll", func_auth.authenticateToken, func_shoppingCart.emptyCart);
 api.delete("/cart/remove", func_auth.authenticateToken, func_shoppingCart.removeFromCart);
-api.delete("/order/", func_order.removeAll);
+api.delete("/order/", func_auth.authenticateToken, func_order.removeAll);
 api.delete("/comment/:id", func_auth.authenticateToken, func_comment.deleteComment);
 
 //API PUT
 api.put("/user/update/:id", func_auth.authenticateToken, func_user.updateUser);
-api.put("/product/update/:id", func_product.update);
-api.put("/cart/update/:orderID", func_shoppingCart.reduceStock);
+api.put("/product/update/:id", func_auth.authenticateToken, func_product.update);
+api.put("/cart/update/:orderID", func_auth.authenticateToken, func_shoppingCart.reduceStock);
 api.put("/order/update/:id", func_auth.authenticateToken, func_order.updateOrderStatus)
 api.put("/comment/update/:id", func_auth.authenticateToken, func_comment.updateComment);
 
